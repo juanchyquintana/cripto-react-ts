49b01ecb55659f3b454570820021bf7a0a686e1c
Juan Diego <quintanajuanchy25@gmail.com>
2024-06-10 10:48:55 -0300

    se agregaron las funciones que faltaban dentro del componente de form

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,14 +1,18 @@
 import { useCryptoStore } from "../store";
 import { currencies } from "../data";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Pair } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies);
+  const fetchData = useCryptoStore((state) => state.fetchData);
+
   const [pair, setPair] = useState<Pair>({
     currency: "",
     criptoCurrency: "",
   });
+  const [error, setError] = useState("");
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setPair({
@@ -17,11 +21,29 @@ export default function CriptoSearchForm() {
     });
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(pair).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+    setError("");
+
+    fetchData(pair);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onClick={handleSubmit}>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
-        <select name="currency" id="currency" onChange={handleChange}>
+        <select
+          name="currency"
+          id="currency"
+          onChange={handleChange}
+          value={pair.currency}
+        >
           <option value="">-- Seleccione --</option>
           {currencies.map((currency) => (
             <option value={currency.code} key={currency.code}>
@@ -37,6 +59,7 @@ export default function CriptoSearchForm() {
           name="criptoCurrency"
           id="criptoCurrency"
           onChange={handleChange}
+          value={pair.criptoCurrency}
         >
           <option value="">-- Seleccione --</option>
           {cryptocurrencies.map((crypto) => (
